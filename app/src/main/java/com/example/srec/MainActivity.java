18fc1d5df702e613fbf1d312ebd5345616af365e
Tongli Zhou <tongliz2>
2019-11-30 01:49:58 -0500

    Use volley to make a web request, but still have some problems waiting to be solved: 1. How to override the getHeader Message in Volley?

--- app/src/main/java/com/example/srec/MainActivity.java
+++ app/src/main/java/com/example/srec/MainActivity.java
@@ -11,8 +11,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Instantiate the RequestQueue, use volley to make a web request from the api used, here is
+        // the full contact api that uploading a email or a phone number will result in a response about
+        // the user of the email address(enrich api). The problem here is that the uploading msg should
+        // contain a header indicating the api key, still working how to override the getHeader() function
+        // in volley.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.fullcontact.com/v3/person.enrich";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        System.out.println(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     @Override
@@ -52,4 +89,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*public void onClickListener() {
+        Button search = findViewById(R.id.search);
+        search.setOnClickListener(v -> {
+            onCreate();
+        });
+    }*/
+
 }
