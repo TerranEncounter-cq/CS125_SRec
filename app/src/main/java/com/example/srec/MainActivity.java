e53eac0b5d43f59d699e2a9eba9b890b82caa1f2
TerranEncounter-cq <55056971+TerranEncounter-cq@users.noreply.github.com>
2019-12-08 15:04:06 -0600

    .xml

--- app/src/main/java/com/example/srec/MainActivity.java
+++ app/src/main/java/com/example/srec/MainActivity.java
@@ -1,32 +1,10 @@
 package com.example.srec;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-// import commons-codec-<version>.jar, download from http://commons.apache.org/proper/commons-codec/download_codec.cgi
-import org.apache.commons.codec.binary.Base64;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.AssetFileDescriptor;
 import android.media.MediaRecorder;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -40,25 +18,19 @@ import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.error.VolleyError;
 import com.android.volley.request.SimpleMultiPartRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.apache.commons.codec.binary.Base64;
-import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+
+// import commons-codec-<version>.jar, download from http://commons.apache.org/proper/commons-codec/download_codec.cgi
 
 public class MainActivity extends AppCompatActivity {
     /** four buttons of UI. */
@@ -68,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     private Button search;
 
     private TextView hint;
+    private TextView a;
+    private TextView result;
+    private TextView exist;
     /** MediaRecorder for recording. */
     private MediaRecorder mediaRecorder;
 
@@ -76,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a = findViewById(R.id.A);
+        exist = findViewById(R.id.exist);
 //        mediaRecorder = new MediaRecorder();
 //        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_2_TS);
 //        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -92,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
         }
         hint = findViewById(R.id.Hint);
-        TextView a = findViewById(R.id.A);
+
         record = findViewById(R.id.record);
         record.setOnClickListener(unused -> {
             record.setVisibility(View.INVISIBLE);
@@ -107,11 +84,11 @@ public class MainActivity extends AppCompatActivity {
 //        mediaRecorder.start();
         });
 
-        TextView exist = findViewById(R.id.exist);
+
         search = findViewById(R.id.search);
         search.setVisibility(View.INVISIBLE);
         byte[] finalResult = result;
-        //search.setOnClickListener(unused -> sendApiAuthorization(finalResult));
+        search.setOnClickListener(unused -> sendApiAuthorization(finalResult));
 
         stop = findViewById(R.id.stop);
         stop.setVisibility(View.INVISIBLE);
@@ -130,31 +107,18 @@ public class MainActivity extends AppCompatActivity {
             delete.setVisibility(View.INVISIBLE);
             search.setVisibility(View.INVISIBLE);
         });
-
+        //search.setOnClickListener(unused -> sendApiAuthorization());
+    }
+    private void sendApiAuthorization(byte[] data) {
         String url = "http://identify-us-west-2.acrcloud.com/v1/identify"; // This is a dummy function which returns the POST url for you
         SimpleMultiPartRequest multiPartRequestWithParams = new SimpleMultiPartRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("Response", response);
-                        hint.setText("response: " + response);
-                        // TODO: Do something on success
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // TODO: Handle your error here
-            }
-        });
-
-        // Add the file here
-        File file = new File("/Users/tony/SRec/app/src/main/res/raw/abc.mp3");
-        if (!file.exists()) {
-            exist.setText("don't exist");
-        }
-        multiPartRequestWithParams.addFile("file", "/Users/tony/SRec/app/src/main/res/raw/abc.mp3");
-
-
+                response -> {
+                    Log.d("Response", response);
+                    hint.setText("response: " + response);
+                }, error -> {
+            hint.setText(error.toString());
+        }) {
+        };
         String http_method  = "Post";
         String http_uri = "/v1/identify";
         String timestamp = getUTCTimeSeconds();
@@ -169,10 +133,10 @@ public class MainActivity extends AppCompatActivity {
                 + timestamp;
         String signature = encryptByHMACSHA1(string_to_sign.getBytes(), "PdxqdTupBdTbGM25es9KzZwM0REiyeLZGBZJNOz7".getBytes());
         // Add the params here
-        multiPartRequestWithParams.addStringParam("access_key","8b53c894de8426e743a93930d812b9aa");
-        multiPartRequestWithParams.addStringParam("data_type", "audio");
-        multiPartRequestWithParams.addStringParam("sample_bytes", result.length + "");
-        multiPartRequestWithParams.addStringParam("sample", new String(result));
+        multiPartRequestWithParams.addStringParam("access_key",access_key);
+        multiPartRequestWithParams.addStringParam("data_type", data_type);
+        multiPartRequestWithParams.addStringParam("sample_bytes", data.length + "");
+        multiPartRequestWithParams.addStringParam("sample", new String(data));
         multiPartRequestWithParams.addStringParam("signature_version", signature_version);
         multiPartRequestWithParams.addStringParam("signature", signature);
         multiPartRequestWithParams.addStringParam("timestamp", timestamp);
@@ -180,11 +144,7 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(multiPartRequestWithParams);
 
-
-
-        //search.setOnClickListener(unused -> sendApiAuthorization());
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -262,7 +222,6 @@ public class MainActivity extends AppCompatActivity {
         return "";
     }
     private byte[] returnByte(InputStream file) {
-        TextView A = findViewById(R.id.result);
         byte[] result = null;
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -284,59 +243,9 @@ public class MainActivity extends AppCompatActivity {
             file.close();
 
         } catch (Exception e) {
-            A.setText(e.toString());
+            a.setText(e.toString());
         }
         System.out.println(result.toString());
         return result;
     }
-
-    protected Map<String,String> getParams(byte[] data) {
-        Map<String,String> params = new HashMap<>();
-        String http_method  = "Post";
-        String http_uri = "/v1/identify";
-        String timestamp = getUTCTimeSeconds();
-        String access_key = "8b53c894de8426e743a93930d812b9aa";
-        String data_type = "audio";
-        String signature_version = "1";
-        String string_to_sign = http_method + "\n"
-                + http_uri + "\n"
-                + access_key + "\n"
-                + data_type + "\n"
-                + signature_version + "\n"
-                + timestamp;
-        String signature = encryptByHMACSHA1(string_to_sign.getBytes(), "PdxqdTupBdTbGM25es9KzZwM0REiyeLZGBZJNOz7".getBytes());
-        params.put("access_key","8b53c894de8426e743a93930d812b9aa");
-        params.put("data_type", "audio");
-        params.put("sample_bytes", data.length + "");
-        params.put("sample", new String(data));
-        params.put("signature_version", signature_version);
-        params.put("signature", signature);
-        params.put("timestamp", timestamp);
-        return params;
-    }
-    /*public String recognize(String host, String accessKey, String secretKey, byte[] queryData, String queryType, int timeout) {
-        String method = "POST";
-        String httpURL = "/v1/identify";
-        String dataType = queryType;
-        String sigVersion = "1";
-        String timestamp = getUTCTimeSeconds();
-
-        String reqURL = "http://" + host + httpURL;
-
-        String sigStr = method + "\n" + httpURL + "\n" + accessKey + "\n" + dataType + "\n" + sigVersion + "\n" + timestamp;
-        String signature = encryptByHMACSHA1(sigStr.getBytes(), secretKey.getBytes());
-
-        Map<String, Object> postParams = new HashMap<String, Object>();
-        postParams.put("access_key", accessKey);
-        postParams.put("sample_bytes", queryData.length + "");
-        postParams.put("sample", queryData);
-        postParams.put("timestamp", timestamp);
-        postParams.put("signature", signature);
-        postParams.put("data_type", dataType);
-        postParams.put("signature_version", sigVersion);
-
-        String res = postHttp(reqURL, postParams, timeout);
-
-        return res;
-    }*/
 }
